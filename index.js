2f74e9e85edbeadf378726fc204c323c6c5e5477
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-07-03 21:58:06 +0300

    build: add html and js files

---
+++ index.js
@@ -0,0 +1,17 @@
+import "https://unpkg.com/react@18/umd/react.development.js";
+import "https://unpkg.com/react-dom@18/umd/react-dom.development.js";
+
+import init, { App, WasmReact } from "./pkg/tetris_with_wasm.js";
+
+await init();
+
+WasmReact.useReact(window.React);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  React.createElement(App, {
+    width: 10,
+    height: 25,
+  })
+);
