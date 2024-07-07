3c6217e7ebff1739e35f64e4b0fd2bf4516b83bd
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-07-07 14:22:55 +0300

    build: update structure

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import "https://unpkg.com/react@18/umd/react.development.js";
 import "https://unpkg.com/react-dom@18/umd/react-dom.development.js";
 
-import init, { App, WasmReact } from "../pkg/tetris.js";
+import init, { App, WasmReact } from "./pkg/tetris.js";
 
 await init();
 
