818fa43039bac10085d1a05de12fc55707ca2d17
Bohdan <76702178+bohdan-mykhailenko@users.noreply.github.com>
2024-07-07 14:17:46 +0300

    build: add pkg folder

---
+++ pkg/tetris_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function App(a: number, b: number): void;
+export function __wbg___wasmreact_componentwrapper_free(a: number): void;
+export function __wasmreact_componentwrapper_render(a: number): number;
+export function __wasmreact_memocomponentwrapper_eq(a: number, b: number): number;
+export function __wbg___wasmreact_memocomponentwrapper_free(a: number): void;
+export function __wasmreact_memocomponentwrapper_render(a: number): number;
+export function __wbg___wasmreact_refcontainervalue_free(a: number): void;
+export function wasmreact_useReact(a: number): void;
+export function __wbg_wasmreact_free(a: number): void;
+export const __wbindgen_export_0: WebAssembly.Table;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h84493b43ff90243e(a: number, b: number): number;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h9ea85429d94ebdcb(a: number, b: number): void;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__hc6c1d772358aa7a6(a: number, b: number, c: number): number;
+export function _dyn_core__ops__function__FnMut__A____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h121525b19269b431(a: number, b: number, c: number): void;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
+export function wasm_bindgen__convert__closures__invoke1_mut_ref__hf6805b9dbe95b53d(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
